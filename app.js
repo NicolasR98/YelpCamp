3cc03e76dd5f4eda69bafe5376ed0d1915a51f50
NicolasR98 <dnicolas.r98@gmail.com>
2020-12-15 18:28:41 +0100

    Add 'ejs-mate' module

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 //--Models
 const Campground = require("./models/campgrounds");
@@ -26,6 +27,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
 
 //--Routes
 //Home
