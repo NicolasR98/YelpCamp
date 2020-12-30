33db45d707e5268319e2a233223bb1a99675d876
NicolasR98 <dnicolas.r98@gmail.com>
2020-12-30 16:28:46 +0100

    Add campground delete middleware

--- models/campgrounds.js
+++ models/campgrounds.js
@@ -1,6 +1,7 @@
 //--Mongoose
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
+const Review = require("./review");
 
 //--Schema
 const campgroundSchema = new Schema({
@@ -15,6 +16,16 @@ const campgroundSchema = new Schema({
       ref: "Review",
     },
   ],
+});
+//Delete middleware
+campgroundSchema.post("findOneAndDelete", async function (doc) {
+  if (doc) {
+    await Review.deleteMany({
+      _id: {
+        $in: doc.reviews,
+      },
+    });
+  }
 });
 
 //--Exports
