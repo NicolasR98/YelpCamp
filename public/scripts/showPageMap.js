84eecc4a695f2d38316c578ccf6e559830cda361
NicolasR98 <dnicolas.r98@gmail.com>
2021-01-24 11:24:38 +0100

    Add marker with description to campgrounds locations in mapbox

--- public/scripts/showPageMap.js
+++ public/scripts/showPageMap.js
@@ -2,9 +2,16 @@ mapboxgl.accessToken = mbxToken;
 var map = new mapboxgl.Map({
   container: "map",
   style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
-  center: campGeometry.coordinates, // starting position [lng, lat]
+  center: campground.geometry.coordinates, // starting position [lng, lat]
   zoom: 15, // starting zoom
 });
 
 new mapboxgl.Marker()
-  .setLngLat(campGeometry.coordinates).addTo(map);
+  .setLngLat(campground.geometry.coordinates)
+  .setPopup(
+    new mapboxgl.Popup({ offset: 25 }).setHTML(`
+      <h5>${campground.title}</h5>
+      <p>${campground.location}</p>
+    `)
+  )
+  .addTo(map);
