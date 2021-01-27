fa66c6a651115d14eaf1a99d3537f3b9874367ff
NicolasR98 <dnicolas.r98@gmail.com>
2021-01-27 11:17:41 +0100

    Clean up template styles

--- public/scripts/showPageMap.js
+++ public/scripts/showPageMap.js
@@ -5,6 +5,7 @@ var map = new mapboxgl.Map({
   center: campground.geometry.coordinates, // starting position [lng, lat]
   zoom: 15, // starting zoom
 });
+map.addControl(new mapboxgl.NavigationControl(), "bottom-right");
 
 new mapboxgl.Marker()
   .setLngLat(campground.geometry.coordinates)
